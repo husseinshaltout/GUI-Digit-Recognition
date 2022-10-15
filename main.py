5d0aa3bdf468c3d52e0d60c762e0c6e36e604064
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2022-10-15 02:26:23 +0200

    fix(classifier): changed reshape to 2D 1, 784

--- main.py
+++ main.py
@@ -6,27 +6,32 @@ import matplotlib.pyplot as plt
 from keras.models import load_model
 import cv2
 
+
 class DigitsClassifier(Frame):
     """Handwritten digits classifier class"""
+
     def __init__(self, parent):
         Frame.__init__(self, parent)
         self.parent = parent
         self.color = "black"
         self.brush_size = 12
         self.setUI()
+
     def set_color(self, new_color):
         """Additional brush color change"""
         self.color = new_color
+
     def set_brush_size(self, new_size):
-            """Changes brush size for testing different lines width"""
-            self.brush_size = new_size
+        """Changes brush size for testing different lines width"""
+        self.brush_size = new_size
+
     def draw(self, event):
-            """Method to draw"""
-            self.canv.create_oval(event.x - self.brush_size,
-                                  event.y - self.brush_size,
-                                  event.x + self.brush_size,
-                                  event.y + self.brush_size,
-                                  fill=self.color, outline=self.color)
+        """Method to draw"""
+        self.canv.create_oval(event.x - self.brush_size,
+                              event.y - self.brush_size,
+                              event.x + self.brush_size,
+                              event.y + self.brush_size,
+                              fill=self.color, outline=self.color)
 
     def save(self):
         """Save the current canvas state as the postscript
@@ -35,9 +40,11 @@ class DigitsClassifier(Frame):
         ps = self.canv.postscript(colormode='mono')
         img = Image.open(io.BytesIO(ps.encode('utf-8')))
         img.save('result.png')
-        a = DigitsClassifier.classify()
-        print(a)
-        self.show_digit(a)
+        # a = DigitsClassifier.classify()
+        print(f'DigitsClassifier:')
+        self.show_digit(self.classify())
+        # self.show_digit(a)
+
     @staticmethod
     def classify():
         """
@@ -48,12 +55,13 @@ class DigitsClassifier(Frame):
         im = cv2.imread("result.png", 0)
         im2 = cv2.resize(im, (28, 28))
         im = im2.reshape(28, 28, -1)
-        im = im.reshape(1, 1, 28, 28)
+        im = im.reshape(1, 784)
         im = cv2.bitwise_not(im)
         plt.imshow(im.reshape(28, 28), cmap='Greys')
         result = classifier.predict(im)
         a = np.argmax(result)
         return a
+
     def show_digit(self, digit):
         """
         Show the digit on the canvas
@@ -62,13 +70,14 @@ class DigitsClassifier(Frame):
         """
         text_label = Label(self, text=digit)
         text_label.grid(row=0, column=1, padx=5, pady=5)
+
     def setUI(self):
         """Setup for all UI elements"""
         self.parent.title("Digit Recognition")
         self.pack(fill=BOTH, expand=1)
-        self.columnconfigure(3,weight=1)
+        self.columnconfigure(3, weight=1)
         self.rowconfigure(6, weight=1)
-        self.canv = Canvas(self, bg="white",width=140, height=140,)
+        self.canv = Canvas(self, bg="white", width=140, height=140,)
         self.canv.grid(row=1, column=0, rowspan=5,
                        padx=5, pady=5,
                        sticky=E + W + S + N)
@@ -77,7 +86,7 @@ class DigitsClassifier(Frame):
 
         pred_label = Label(self, text='Prediction')
         pred_label.grid(row=0, column=0, padx=5, pady=5)
-        
+
         black_btn = Button(self, text="Draw", width=10,
                            command=lambda: self.set_color("black"))
         black_btn.grid(row=1, column=1)
@@ -87,16 +96,19 @@ class DigitsClassifier(Frame):
         clear_btn = Button(self, text="Clear all", width=10,
                            command=lambda: self.canv.delete("all"))
         clear_btn.grid(row=3, column=1)
-        
-        
+
         done_btn = Button(self, text="Predict", width=10,
                           command=lambda: self.save())
         done_btn.grid(row=4, column=1)
+
+
 def main():
     root = Tk()
     root.geometry("280x200")
     root.resizable(1, 1)
     app = DigitsClassifier(root)
     root.mainloop()
+
+
 if __name__ == '__main__':
     main()
